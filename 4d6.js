a9fe602937b244a2eb9f06e5bffe7744c19e7736
Shane Morales <shane.morales@gmail.com>
2017-09-07 12:41:23 -0400

    started work on index.html, main.css

--- 4d6.js
+++ 4d6.js
@@ -1,5 +1,7 @@
 var arrScore = [];
 
+var scrNames = ["Strength","Dexterity","Constitution","Intelligence","Wisdom","Charisma"];
+
 function rndSix() {
   return Math.floor(Math.random() * 6) +1;
 }
@@ -24,6 +26,37 @@ for (i = 1;i < 7;i++) {
 
 }
 
-console.log(arrScore);
+/* console.log(arrScore); */
 
-
+let fnlScores = [
+  {
+    ability: scrNames[0],
+    score: arrScore[0]
+  },
+  {
+    ability: scrNames[1],
+    score: arrScore[1]
+  },
+  {
+    ability: scrNames[2],
+    score: arrScore[2]
+  },
+  {
+    ability: scrNames[3],
+    score: arrScore[3]
+  },
+  {
+    ability: scrNames[4],
+    score: arrScore[4]
+  },
+  {
+    ability: scrNames[5],
+    score: arrScore[5]
+  }
+  ];
+  
+  for (j=0;j<fnlScores.length;j++) {
+    for (let x in fnlScores[j]) {
+      console.log(fnlScores[j][x]);
+    }
+}
